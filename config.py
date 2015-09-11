f3550035ce89971fbf7e45815ee782c6c34fa2a3
JinruiWang <306090773@qq.com>
2015-09-11 19:46:06 +0800

    修改到5000端口测试

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
     PASSWORD_PREFIX = "08b3db21-d120-11e4-9ttd-10ddb199c373"
     ENCRYPT_PWD_URL = None
     SERVER_IP = '0.0.0.0'
-    SERVER_PORT = 80
+    SERVER_PORT = 5000
 
 
 class ProductionConfig(Config):
